c6de4fc6ca0976439e11e0fb7a7962077a194d80
Evan <pifan76@gmail.com>
2018-11-03 00:11:57 +0800

    Initial commit

---
+++ app/src/main/java/evan/chen/tutorial/vectordrawablesample/MainActivity.kt
@@ -0,0 +1,12 @@
+package evan.chen.tutorial.vectordrawablesample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
